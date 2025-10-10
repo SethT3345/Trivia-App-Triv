f383ead33c70924a586d4e16919c6f3077046c6a
Korman Carter <kormancarter@gmail.com>
2025-10-10 09:27:43 -0600

    signup js page complete might need to make a few changes

---
+++ signup.js
@@ -0,0 +1,32 @@
+function handleSignup(){
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    
+    // Get existing users from localStorage
+    const existingUsers = JSON.parse(localStorage.getItem('users')) || [];
+    const userExists = existingUsers.find(user => user.username === username);
+    if(userExists){
+        alert("User Already Exists")
+        throw new Error("That User Already Exists");        
+    }
+    if(!username.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)){
+            alert("Enter A Valid Email")
+            throw new Error("Doesnt Match An Email!");
+        }
+    if(!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])(?=.{8,})/)){
+        alert("Enter A Stronger Password")
+        throw new Error("Not strong enough password!");
+    }
+
+
+
+    const newUser = { username: username, password: password };
+    existingUsers.push(newUser);
+    localStorage.setItem('users', JSON.stringify(existingUsers));
+    
+    localStorage.setItem('currentUser', JSON.stringify(newUser));
+    
+    alert("Account created successfully!");
+
+    window.location.href = "login.html";
+}
