c0fcf3afb96fc9831fb357cfe7a8041ab2d4e2a0
Seth Tomlinson <sjt2908@gmail.com>
2025-10-27 08:25:31 -0600

    Update print statement from 'Hello' to 'Goodbye'

--- wordle.js
+++ wordle.js
@@ -438,4 +438,9 @@ function announceWinner() {
 
 document.getElementById('playAgainBtn').addEventListener('click', () => {
     location.reload();
+
 });
+
+function gohp(){
+    window.location.href = "index.html";
+}
