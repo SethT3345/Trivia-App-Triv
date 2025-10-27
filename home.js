6a44ba33498fd45fad3cf2960c8541ba36d08dfe
Seth Tomlinson <sjt2908@gmail.com>
2025-10-27 08:16:32 -0600

    Refactor resetTwoPlayer function and event listeners

--- home.js
+++ home.js
@@ -2,21 +2,19 @@
   function resetTwoPlayer(){
     localStorage.clear();
   }
-
   const playSelectors = ['.two-player-play', '#playBtn', '#playAgainBtn', '#start2P'];
-
   playSelectors.forEach(sel => {
     document.addEventListener('click', (e) => {
       const btn = e.target.closest(sel);
       if (btn) {
-        resetTwoPlayerStateLocal();
+        resetTwoPlayer();
       }
     });
   });
-
   window.resetTwoPlayerState = resetTwoPlayer;
 })();
 
 function gohp(){
     window.location.href = "index.html";
+
 }
